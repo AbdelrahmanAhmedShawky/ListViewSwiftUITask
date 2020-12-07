ec7c15379c0038b7bc41af699571f45e41b7bd0b
Abdelrahman Shawky <ashawky@egypt.itgsolutions.com>
2020-12-07 08:59:59 +0200

    Initial Commit

---
+++ ListViewTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewTask
+//
+//  Created by Abdelrahman Shawky on 12/7/20.
+//  Copyright © 2020 Abdelrahman Shawky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
